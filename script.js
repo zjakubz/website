adc077c8e8723cf64601d06e3116ce45d314b6ba
zjakubz <98275012+zjakubz@users.noreply.github.com>
2024-07-24 13:18:47 +0200

    script.js

--- script.js
+++ script.js
@@ -2,11 +2,29 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const button = document.getElementById('myButton');
   
     button.addEventListener('click', () => {
-      alert('Button clicked!');
+      alert('Webová stránka byla už otevřena');
     });
   
     // Add touch event listener
     button.addEventListener('touchstart', () => {
-      alert('Button touched!');
+      alert('Webová stránka byla už otevřena!');
     });
   });
+document.addEventListener('DOMContentLoaded', function() {
+    const dropdown = document.querySelector('.dropdown');
+    const dropbtn = document.querySelector('.dropbtn');
+
+    dropbtn.addEventListener('click', function(event) {
+        event.stopPropagation();
+        dropdown.classList.toggle('show');
+    });
+
+    // Close the dropdown if the user clicks outside of it
+    window.addEventListener('click', function(event) {
+        if (!event.target.matches('.dropbtn')) {
+            if (dropdown.classList.contains('show')) {
+                dropdown.classList.remove('show');
+            }
+        }
+    });
+});
